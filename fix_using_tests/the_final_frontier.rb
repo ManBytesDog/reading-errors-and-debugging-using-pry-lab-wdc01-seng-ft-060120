cfd2770a649079ec3ac4df2a0981d33a63251b20
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-04-23 02:48:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,16 +4,11 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-binding.pry
-
 def state_log(generate_star_date)
   "Captain's Log, star date #{generate_star_date}."
 end
-
-binding.pry
 
 def engage
   date = generate_star_date
   puts state_log(date)
 end
-binding.pry
